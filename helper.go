5463dc26ccba132322db6484a2517861481ad71f
Michael Wolz <michaelwolz93@gmail.com>
2018-12-17 07:54:28 +0100

    It's working

--- helper.go
+++ helper.go
@@ -77,7 +77,7 @@ var redirectNeighbors = map[string]map[string]string{
 	"BOX_A4": {"BOX_A1": "BOX_A7", "BOX_A7": "BOX_A1"},
 	"BOX_A7": {},
 	"BOX_D1": {"BOX_A1": "BOX_G1", "BOX_G1": "BOX_A1"},
-	"BOX_D4": {"BOX_A4": "BOX_G4", "BOX_D1": "BOX_D7"},
+	"BOX_D4": {"BOX_A4": "BOX_G4", "BOX_D1": "BOX_D7", "BOX_G4": "BOX_A4", "BOX_D7": "BOX_D1"},
 	"BOX_D7": {"BOX_A7": "BOX_G7", "BOX_G7": "BOX_A7"},
 	"BOX_G1": {},
 	"BOX_G4": {"BOX_G1": "BOX_G7", "BOX_G7": "BOX_G1"},
--- message.go
+++ message.go
@@ -5,29 +5,34 @@ import (
 	"log"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 var msgProcessRegex = regexp.MustCompile(`^(BOX_[A,D,G][1,4,7]),([0-2]),([0-2]):([1-9])$`)
 var msgRegex = regexp.MustCompile(`^BOX_[A,D,G][1,4,7],[0-2],[0-2]:[1-9]$`)
+var msgChan = make(chan string, 100)
 
-func processMessage(message string) {
-	fmt.Println("Processing received: " + message)
-	if msgRegex.MatchString(message) {
-		matches := msgProcessRegex.FindStringSubmatch(message)
+func processMessages() {
+	for message := range msgChan {
+		message = strings.TrimSuffix(message, "\n")
+		fmt.Println("Processing: " + message)
+		if msgRegex.MatchString(message) {
+			matches := msgProcessRegex.FindStringSubmatch(message)
 
-		val, err := strconv.Atoi(matches[4])
-		checkSoftErr(err)
-		if matches[1][:len(matches[1])-1] == myBox.id[:len(myBox.id)-1] {
-			x, err := strconv.Atoi(matches[2])
+			val, err := strconv.Atoi(matches[4])
 			checkSoftErr(err)
-			myBox.setColValue(x, val)
+			if matches[1][:len(matches[1])-1] == myBox.id[:len(myBox.id)-1] {
+				x, err := strconv.Atoi(matches[2])
+				checkSoftErr(err)
+				myBox.setColValue(x, val)
+			} else {
+				y, err := strconv.Atoi(matches[3])
+				checkSoftErr(err)
+				myBox.setRowValue(y, val)
+			}
+			redirectToNeighbor(message, matches[1])
 		} else {
-			y, err := strconv.Atoi(matches[3])
-			checkSoftErr(err)
-			myBox.setRowValue(y, val)
+			log.Println("malformed message received " + message)
 		}
-		redirectToNeighbor(message, matches[1])
-	} else {
-		log.Println("malformed message received " + message)
 	}
 }
